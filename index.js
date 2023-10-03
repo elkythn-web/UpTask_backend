56f12de6c37041a16a70bb785244c61da0895522
elkythn-web <elkinportillo5@gmail.com>
2023-10-02 22:36:50 -0600

    Update Cors List good

--- index.js
+++ index.js
@@ -18,15 +18,16 @@ connectDB();
 // Enable CORS
 const whitelist = [process.env.FRONTEND_URL];
 const corsOptions = {
-    origin: (origin, callback) => {
-        const existe = whitelist.some(dominio => dominio === origin);
-        if (existe) {
-            callback(null, true);
-        } else {
-            callback(new Error('No permitido por CORS'));
-        }
-    }
-}
+    origin: function (origin, callback) {
+      if (whitelist.includes(origin)) {
+        // Puede consultar la API
+        callback(null, true);
+      } else {
+        // No esta permitido
+        callback(new Error("Error de Cors"));
+      }
+    },
+  };
 app.use(cors(corsOptions));
 
 
